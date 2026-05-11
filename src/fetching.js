0606661fab2483b1d0b92a3342c0f7c2e6391c49
mahmoudbitar02 <bitarmahmoud909@gmail.com>
2026-05-11 21:59:52 +0200

    change fetch to https

--- src/fetching.js
+++ src/fetching.js
@@ -1,7 +1,7 @@
 import { API_KEY } from "./keys.js";
 
 const KEY = API_KEY;
-const BASE_URL = "http://api.weatherapi.com/v1";
+const BASE_URL = "https://api.weatherapi.com/v1";
 
 export async function fetchWeatherForecastData(city, day) {
   const response = await fetch(`${BASE_URL}/forecast.json?key=${KEY}&q=id:${city}&lang=de&days=${day}`);
